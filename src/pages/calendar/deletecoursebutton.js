b2ebbfcbc734290c6ddae8cccbd2cee0dedfecfe
josue123455 <andaluz1498@Gmail.com>
2023-11-09 00:47:30 -0500

    Update deletecoursebutton.js

--- src/pages/calendar/deletecoursebutton.js
+++ src/pages/calendar/deletecoursebutton.js
@@ -1,94 +1,125 @@
-//button compnent to add a delete course to the system 
+//button compnent to add a new course to the system 
 //checklist 
 //1. create component- done 
-//2. create feilds - done
-//3. add it to the inputdata.js - 
+//2. create feilds - done 
+//3. add it to the inputdata.js - done
 //4. add logic to for PUT
 
 
 import React from 'react';
+import CoursenumberDropdown from './coursenumberdropdown'; // Replace 'path/to' with the correct path to your CoursenumberDropdown component
+import {deleteCourse } from '../../functions/http'; // Import the updateCourse method
 
-//using the state button when the button is not clicked the state does not show the text boxes
-class Deletecoursebutton extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        showForm: false, // State to control form visibility
-        subject: '',
-        courseNumber: '',
-        title: '',
-        description: ''
-      };
-    }
-  
-    // Function to handle form input changes
-    handleInputChange = (event) => {
-      const { name, value } = event.target;
-      this.setState({ [name]: value });
+
+class UpdateCourseButtonComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false, // State to control form visibility
+      courseNumber: '',
+      title: '',
+      subject: '',
+      description: '',
+      id: null,
     };
-  
-    // Function to handle form submission
-  // Function to handle professor update
-  handleDelete = () => {
-    // Handle professor deletion logic (e.g., send delete request to API)  Handle the API logic here FR
-    console.log('Deleting Professor:', this.state.professorName);
-    // Reset the form and hide it after deletionubmission
-      this.setState({ showForm: false, subject: '', courseNumber: '', title: '', description: '' });
-    };
-  
-    render() {
-      return (
-        <div className="button-container">
-          <button className="big-button" onClick={() => this.setState({ showForm: true })}>
-            Delete Course
-          </button>
-  
-          {this.state.showForm && (
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                subject:
-                <input
-                  type="text"
-                  name="subject"
-                  value={this.state.subject}
-                  onChange={this.handleInputChange}
-                />
-              </label>
-              <label>
-                courseNumber:
-                <input
-                  type="text" // remeber to make these required as rommel so they are imported with no issues
-                  name="courseNumber"
-                  value={this.state.courseNumber}
-                  onChange={this.handleInputChange}
-                />
-              </label>
-              <label>
-                title:
-                <input
-                  type="text" // remeber to make these required as rommel so they are imported with no issues
-                  name="title"
-                  value={this.state.title}
-                  onChange={this.handleInputChange}
-                />
-              </label>
-              <label>
-                description:
-                <input
-                  type="text" // remeber to make these required as rommel so they are imported with no issues
-                  name="professorRank"
-                  value={this.state.description}
-                  onChange={this.handleInputChange}
-                />
-              </label>
-              {this.state.showForm && (
-          <button onClick={this.handleDelete}>Delete Course</button>
-          )}
-            </form>
-          )}
-        </div>
-      );
-    }
   }
-  
-  export default Deletecoursebutton;
+
+  // Function to handle form input changes
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  // Function to handle selected course
+  onSelectCourse = (course) => {
+    this.setState({
+      courseNumber: course.courseNumber,
+      title: course.title,
+      subject: course.title,
+      description: course.description,
+      id: course._id,
+    });
+  };
+
+  // Function to handle course update
+  handleDelete = async (event) => {
+    event.preventDefault();
+
+    if (this.state.courseNumber && this.state.title) {
+      await deleteCourse(this.state.id, {
+        courseNumber: this.state.courseNumber,
+        title: this.state.title,
+        subject: this.state.subject,
+        description: this.state.description,
+        _id: this.state.id,
+      });
+      this.setState({
+        showForm: false,
+        courseNumber: '',
+        subject: '',
+        title: '',
+        description: '',
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="button-container">
+        <button className="big-button" onClick={() => this.setState({ showForm: true })}>
+          Delete Course
+        </button>
+
+        {this.state.showForm && (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Select Course:
+              {/* Replace the input field with the CoursenumberDropdown component */}
+              <CoursenumberDropdown onSelectCourse={this.onSelectCourse} />
+            </label>
+            <label>
+              Update Course Number:
+              <input
+                type="text"
+                name="courseNumber"
+                value={this.state.courseNumber}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <label>
+              Update Course Title:
+              <input
+                type="text"
+                name="title"
+                value={this.state.title}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <label>
+              Update Course Subject:
+              <input
+                type="text"
+                name="description"
+                value={this.state.description}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <label>
+              Update Course Description:
+              <input
+                type="text"
+                name="subject"
+                value={this.state.subject}
+                onChange={this.handleInputChange}
+              />
+            </label>
+
+            {this.state.showForm && <button onClick={this.handleDelete}>Delete Course</button>}
+          </form>
+        )}
+      </div>
+    );
+  }
+}
+
+export default UpdateCourseButtonComponent;
