dd3ce796ac1c7af1c188a608f2e8b7e7deb5cec9
josue123455 <andaluz1498@Gmail.com>
2023-11-21 00:59:16 -0500

    Revert "Update InputData.js"
    This reverts commit efad7a6ee4d82ea3f98045ee852bbc9a66289c91.

--- src/pages/inputData/InputData.js
+++ src/pages/inputData/InputData.js
@@ -16,14 +16,13 @@ import DeleteSectionbutton from '../calendar/sectionDeleteButton.js';
 import { Link } from 'react-router-dom';
 
 class InputData extends React.Component {
-  //tehe constructor where the intial state is set to professor since this this is the first tab that is shown
   constructor(props) {
     super(props);
     this.state = {
       activeTab: 'professor',
     };
   }
-//handle tab change method(since associated with the class personal notes) when a tab is clicked the method is called and setting the active tab to the selected tab and triggers a re-render
+
   handleTabChange = (selectedTab) => {
     this.setState({
       activeTab: selectedTab,
@@ -34,91 +33,98 @@ class InputData extends React.Component {
     const { activeTab } = this.state;
 
     return (
-        //the component uses tge tab container from the react bootstrap library  the nav key is used to create navigation tabs and each nav item represents a tab the event key prop is used to the coresponding tab key for the tab
-      <Tab.Container id="data-tabs" activeKey={activeTab} onSelect={this.handleTabChange}>
-        <Nav variant="tabs">
-          <Nav.Item>
-            <Nav.Link eventKey="professor">Professor</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="course">Course</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="room">Room</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="section">Section</Nav.Link>
-          </Nav.Item>
-        </Nav>
+      <div>
 
-        <Tab.Content>
-          <Tab.Pane eventKey="professor">
-            <div className='card'>
-              <div className='card-header'>
-                <h2>Professor</h2>
+        <div style={{ margin: '10px' }}>
+          <Link to="/">
+            <button>Go back to Calendar View</button>
+          </Link>
+        </div>
+        {/* Tab Container */}
+        <Tab.Container id="data-tabs" activeKey={activeTab} onSelect={this.handleTabChange}>
+          <Nav variant="tabs">
+            <Nav.Item>
+              <Nav.Link eventKey="professor">Professor</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="course">Course</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="room">Room</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="section">Section</Nav.Link>
+            </Nav.Item>
+          </Nav>
+
+          <Tab.Content>
+            <Tab.Pane eventKey="professor">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Professor</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'professor' && (
+                    <>
+                      <AddProfessorButton />
+                      <UpdateProfessorButtonComponent />
+                      <DeleteprofessorbuttonComponent />
+                    </>
+                  )}
+                </div>
               </div>
-              <div className='card-body'>
-                {activeTab === 'professor' && (
-                  <>
-                    <AddProfessorButton />
-                    <UpdateProfessorButtonComponent />
-                    <DeleteprofessorbuttonComponent />
-                  </>
-                )}
+            </Tab.Pane>
+            <Tab.Pane eventKey="course">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Course</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'course' && (
+                    <>
+                      <Addcoursebutton />
+                      <Updatecoursebutton />
+                      <Deletecoursebutton />
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-          </Tab.Pane>
-          <Tab.Pane eventKey="course">
-            <div className='card'>
-              <div className='card-header'>
-                <h2>Course</h2>
+            </Tab.Pane>
+            <Tab.Pane eventKey="room">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Room</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'room' && (
+                    <>
+                      <Addroombutton />
+                      <Updateroombutton />
+                      <Deleteroombutton />
+                    </>
+                  )}
+                </div>
               </div>
-              <div className='card-body'>
-                {activeTab === 'course' && (
-                  <>
-                    <Addcoursebutton />
-                    <Updatecoursebutton />
-                    <Deletecoursebutton />
-                  </>
-                )}
+            </Tab.Pane>
+            <Tab.Pane eventKey="section">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Section</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'section' && (
+                    <>
+                      <AddSectionbutton />
+                      <UpdateSectionButton />
+                      <DeleteSectionbutton />
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-            {/* inside each tab pane there is conditonal rendering buttions based on the active tab this ensures that the buttions for the current tab are being displayed */}
-          </Tab.Pane>
-          <Tab.Pane eventKey="room">
-            <div className='card'>
-              <div className='card-header'>
-                <h2>Room</h2>
-              </div>
-              <div className='card-body'>
-                {activeTab === 'room' && (
-                  <>
-                    <Addroombutton />
-                    <Updateroombutton />
-                    <Deleteroombutton />
-                  </>
-                )}
-              </div>
-            </div>
-          </Tab.Pane>
-          <Tab.Pane eventKey="section">
-            <div className='card'>
-              <div className='card-header'>
-                <h2>Section</h2>
-              </div>
-              <div className='card-body'>
-                {activeTab === 'section' && (
-                  <>
-                    <AddSectionbutton />
-                    <UpdateSectionButton />
-                    <DeleteSectionbutton />
-                  </>
-                )}
-              </div>
-            </div>
-          </Tab.Pane>
-        </Tab.Content>
-      </Tab.Container>
+            </Tab.Pane>
+          </Tab.Content>
+        </Tab.Container>
+      </div>
     );
   }
 }
