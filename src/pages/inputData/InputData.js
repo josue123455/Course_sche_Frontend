3dbac2aaad5d2af255e3a570795cec997f1cd31a
Rommel Trejo <rommeltrejo@gmail.com>
2023-11-21 23:22:29 -0500

    import/export fix merge conflict

--- src/pages/inputData/InputData.js
+++ src/pages/inputData/InputData.js
@@ -13,8 +13,8 @@ import Deleteroombutton from '../calendar/roomDeleteButton.js';
 import AddSectionbutton from '../calendar/sectionAddButton.js';
 import UpdateSectionButton from '../calendar/sectionUpdateButton.js';
 import DeleteSectionbutton from '../calendar/sectionDeleteButton.js';
-import ManageDataButton from './buttons/ManageDataButton.js';
 import { Link } from 'react-router-dom';
+import ManageDataButton from './buttons/ManageDataButton.js';
 
 class InputData extends React.Component {
   constructor(props) {
@@ -35,7 +35,6 @@ class InputData extends React.Component {
 
     return (
       <div>
-
         <div style={{ margin: '10px' }}>
           <Link to="/">
             <button>Go back to Calendar View</button>
@@ -56,76 +55,93 @@ class InputData extends React.Component {
             <Nav.Item>
               <Nav.Link eventKey="section">Section</Nav.Link>
             </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="export">Export/Import</Nav.Link>
+            </Nav.Item>
           </Nav>
 
-        <Tab.Content>
-          <Tab.Pane eventKey="professor">
-            <div className='card'>
-              <div className='card-header'>
-                <h2>Professor</h2>
+          <Tab.Content>
+            <Tab.Pane eventKey="professor">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Professor</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'professor' && (
+                    <>
+                      <AddProfessorButton />
+                      <UpdateProfessorButtonComponent />
+                      <DeleteprofessorbuttonComponent />
+                    </>
+                  )}
+                </div>
               </div>
-              <div className='card-body'>
-                {activeTab === 'professor' && (
-                  <>
-                    <AddProfessorButton />
-                    <UpdateProfessorButtonComponent />
-                    <DeleteprofessorbuttonComponent />
-                  </>
-                )}
+            </Tab.Pane>
+            <Tab.Pane eventKey="course">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Course</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'course' && (
+                    <>
+                      <Addcoursebutton />
+                      <Updatecoursebutton />
+                      <Deletecoursebutton />
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-          </Tab.Pane>
-          <Tab.Pane eventKey="course">
-            <div className='card'>
-              <div className='card-header'>
-                <h2>Course</h2>
+            </Tab.Pane>
+            <Tab.Pane eventKey="room">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Room</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'room' && (
+                    <>
+                      <Addroombutton />
+                      <Updateroombutton />
+                      <Deleteroombutton />
+                    </>
+                  )}
+                </div>
               </div>
-              <div className='card-body'>
-                {activeTab === 'course' && (
-                  <>
-                    <Addcoursebutton />
-                    <Updatecoursebutton />
-                    <Deletecoursebutton />
-                  </>
-                )}
+            </Tab.Pane>
+            <Tab.Pane eventKey="section">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Section</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'section' && (
+                    <>
+                      <AddSectionbutton />
+                      <UpdateSectionButton />
+                      <DeleteSectionbutton />
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-            {/* inside each tab pane there is conditonal rendering buttions based on the active tab this ensures that the buttions for the current tab are being displayed */}
-          </Tab.Pane>
-          <Tab.Pane eventKey="room">
-            <div className='card'>
-              <div className='card-header'>
-                <h2>Room</h2>
+            </Tab.Pane>
+            <Tab.Pane eventKey="export">
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Export/Import</h2>
+                </div>
+                <div className='card-body'>
+                  {activeTab === 'export' && (
+                    <>
+                      <ManageDataButton />
+                    </>
+                  )}
+                </div>
               </div>
-              <div className='card-body'>
-                {activeTab === 'room' && (
-                  <>
-                    <Addroombutton />
-                    <Updateroombutton />
-                    <Deleteroombutton />
-                  </>
-                )}
-              </div>
-            </div>
-          </Tab.Pane>
-          <Tab.Pane eventKey="section">
-            <div className='card'>
-              <div className='card-header'>
-                <h2>Section</h2>
-              </div>
-              <div className='card-body'>
-                {activeTab === 'section' && (
-                  <>
-                    <AddSectionbutton />
-                    <UpdateSectionButton />
-                    <DeleteSectionbutton />
-                  </>
-                )}
-              </div>
-            </div>
-          </Tab.Pane>
-        </Tab.Content>
-      </Tab.Container>
+            </Tab.Pane>
+          </Tab.Content>
+        </Tab.Container>
+      </div>
     );
   }
 }
